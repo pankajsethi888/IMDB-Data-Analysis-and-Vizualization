7cb1ae8f0ceedf6769bbf2fb48dfbc3d470e8709
Pankaj Sethi <pankajsethi888@users.noreply.github.com>
2020-08-21 18:34:36 +0530

    Instead of links the complete json_script part is exported and stored in MovieLinks table. Further operations are done by loading json script using these scripts only.

--- Scraping_top250_movies_Details.py
+++ Scraping_top250_movies_Details.py
@@ -1,6 +1,4 @@
 
-from bs4 import BeautifulSoup
-import requests
 import sqlite3
 import json
 
@@ -10,17 +8,21 @@ cur = conn.cursor()
 cur.executescript('''
                   DROP TABLE IF EXISTS ContentRateList;
                   DROP TABLE IF EXISTS MovieDetails;
+                  DROP TABLE IF EXISTS DirectorList;
+                  DROP TABLE IF EXISTS Directors;
+                  DROP TABLE IF EXISTS Genres;
+                  DROP TABLE IF EXISTS GenreList;
 
                   CREATE TABLE IF NOT EXISTS MovieDetails(
-                      id            INTEGER NOT NULL PRIMARY KEY UNIQUE,
-                      movie_name    TEXT UNIQUE,
-                      director_id         INTEGER,
-                      contRate_id      INTEGER
+                      id                INTEGER NOT NULL PRIMARY KEY UNIQUE,
+                      movie_name        TEXT UNIQUE,
+                      contRate_id       INTEGER
                 );
-
+                /* Genre Moive many-to-many relationship */
                   CREATE TABLE IF NOT EXISTS Genres (
-                      id            INTEGER NOT NULL PRIMARY KEY UNIQUE,
-                      genre_id      INTEGER
+                      movie_id      INTEGER,
+                      genre_id      INTEGER,
+                      PRIMARY KEY   (movie_id, genre_id)
                 );
                   CREATE TABLE IF NOT EXISTS GenreList (
                       id        INTEGER NOT NULL PRIMARY KEY UNIQUE,
@@ -31,36 +33,77 @@ cur.executescript('''
                       id        INTEGER NOT NULL PRIMARY KEY UNIQUE,
                       contRate  TEXT UNIQUE
                 );
-
+                /* Director Movie many-to-many relationship */
+                  CREATE TABLE IF NOT EXISTS Directors (
+                      movie_id      INTEGER,
+                      director_id   INTEGER,
+                      PRIMARY KEY   (movie_id, director_id)
+                );
                   CREATE TABLE IF NOT EXISTS DirectorList (
                       id        INTEGER NOT NULL PRIMARY KEY UNIQUE,
                       director  TEXT UNIQUE
                 );
                   ''')
 
-base_url = 'http://www.imdb.com'
 
 cur.execute('''
-            SELECT movie_id,link FROM MovieLinks ORDER BY movie_id;
+            SELECT movie_id, json_script FROM MovieLinks ORDER BY movie_id;
             ''')
-
 data = cur.fetchall()
 
-for movie_id, link in data:
-    if movie_id >= 3:
-        break
-    print("Retriving Movie: {}".format(movie_id))
-    mv_url = base_url + link
-    response = requests.get(mv_url)
-    soup = BeautifulSoup(response.text, 'lxml')
+def Dir_input(d, movie_id):
+        cur.execute('''
+                    INSERT OR IGNORE INTO DirectorList (director)
+                    VALUES (?)
+                    ''', (d['name'],))
+        cur.execute('''
+                    SELECT id FROM DirectorList WHERE director=?
+                    ''', (d['name'],))
+        director_id = cur.fetchone()[0]
 
-    js_script = soup.select('script[type="application/ld+json"]')[0].contents[0]
-    js = json.loads(js_script)
+        cur.execute('''INSERT OR REPLACE INTO Directors (movie_id, director_id)
+                    VALUES (?,?)''', (movie_id,director_id))
+
+def Dir_list(dirc, movie_id):
+    if type(dirc) is list:
+        for d in dirc:
+            Dir_input(d, movie_id)
+    else:
+        Dir_input(dirc, movie_id)
 
 
-    contRate = js['contentRating']
+def Genre_input(genre, movie_id):
+    cur.execute('''
+                INSERT OR IGNORE INTO GenreList (genre)
+                VALUES (?)''', (genre,))
+    cur.execute('''
+                SELECT id FROM GenreList WHERE genre=?
+                ''', (genre,))
+    genre_id = cur.fetchone()[0]
+
+    cur.execute('''
+                INSERT OR REPLACE INTO Genres (movie_id, genre_id)
+                VALUES (?,?)
+                ''', (movie_id, genre_id))
+
+def Genre_list(g, movie_id):
+    if type(g) is list:
+        for genre in g:
+            Genre_input(genre, movie_id)
+    else:
+        Genre_input(g, movie_id)
+
+
+for movie_id, script in data:
+
+    js = json.loads(script)
+
+    try:
+        contRate = js['contentRating']
+    except:
+        contRate = 'Not Rated'
+
     movie_name = js['name']
-    dirc_name = js['director']['name']
 
     cur.execute('''
                 INSERT OR IGNORE INTO ContentRateList (contRate)
@@ -71,21 +114,15 @@ for movie_id, link in data:
                 ''', (contRate,))
     contRate_id = cur.fetchone()[0]
 
-    cur.execute('''
-                INSERT OR IGNORE INTO DirectorList (director)
-                VALUES (?)
-                ''', (dirc_name,))
-    cur.execute('''
-                SELECT id FROM DirectorList WHERE director=?
-                ''', (dirc_name,))
-    director_id = cur.fetchone()[0]
+    Dir_list(js['director'], movie_id)
 
+    Genre_list(js['genre'], movie_id)
 
     cur.execute('''
-                INSERT OR IGNORE INTO MovieDetails (movie_name, contRate_id, director_id)
-                VALUES (?,?,?)''', (movie_name, contRate_id, director_id))
+                INSERT OR IGNORE INTO MovieDetails (movie_name, contRate_id)
+                VALUES (?,?)''', (movie_name, contRate_id))
 
-print("Retrived All Movies!")
+print("Database Updated!")
 
 conn.commit()
 conn.close()
--- Web-scraping from IMDB.py
+++ Web-scraping from IMDB.py
@@ -11,12 +11,24 @@ links = [a.attrs.get('href') for a in soup.select('td.titleColumn a')]
 
 imdb = {}
 
+base_url = 'http://www.imdb.com'
+
 for i, mvi in enumerate(movies):
+
+    print("Retriving Movie: {}".format(i+1))
+
     movie_str = mvi.get_text()
     movie = (' '.join(movie_str.split()).replace('.', ''))
     title = movie[len(str(i+1))+1:-7]
 
-    imdb[title] = links[i]
+    mv_url = base_url + links[i]
+    response = requests.get(mv_url)
+    soup = BeautifulSoup(response.text, 'lxml')
+    json_script = soup.select('script[type="application/ld+json"]')[0].contents[0]
+
+    imdb[title] = json_script
+
+print("Retrived All Top 250 Movies!!!")
 
 # Load data into SQL database
 import sqlite3
@@ -29,15 +41,18 @@ cur.executescript('''
                   DROP TABLE IF EXISTS MovieLinks;
 
                   CREATE TABLE MovieLinks (
-                      id        INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
-                      title     TEXT UNIQUE,
-                      link      TEXT UNIQUE
+                     movie_id       INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+                     movie_name     TEXT UNIQUE,
+                     json_script
                 );
                   ''')
 
-for k, v in imdb.items():
+for name, script in imdb.items():
     cur.execute('''
-                INSERT OR IGNORE INTO MovieLinks (title, link) VALUES (?,?)
-                ''', (k, v))
+                INSERT OR IGNORE INTO MovieLinks (
+                    movie_name,
+                    json_script)
+                VALUES (?,?)
+                ''', (name, script))
 conn.commit()
 conn.close()
